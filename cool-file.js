a76346b37c89a996b2eda33eb198ebe12165770e
Josh Abernathy <josh@github.com>
2019-01-22 07:48:33 -0500

    a

--- cool-file.js
+++ cool-file.js
@@ -1,1 +1,1 @@
-hey yooooooooabcdefghaasdd
+hey yooooooooabcdefghaasdda
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 require('noodle-it')
 
 const app = express()
-const port = process.env.PORT || 3001
-asdf
+const port = pro 7 8 cess.env.PORT || 3001
+asdf 1 2 3 4 5 6
 app.get('/', (req, res) => {
   console.log('Oh boy!!!!!!!')
   console.info(`that's what's up`)
